6f05ac050a21c9a1edee36a4e6a2813651a306e0
b-01 <>
2019-01-23 21:36:19 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("LICENSE", "r") as fh:
-    license_txt = fh.read()
-
 setuptools.setup(
     name="lzutf8",
     version="0.1",
@@ -15,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/b-01/lzutf8.py",
     packages=setuptools.find_packages(),
-    license=license_txt,
+    data_files = [("", ["LICENSE"])],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
